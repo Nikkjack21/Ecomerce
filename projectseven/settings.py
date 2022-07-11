7f4ba20505cd6c3697dcbb47c3e82ecce28a8c29
Nikkjack21 <jackson97patrick@gmail.com>
2022-07-11 21:43:48 +0530

    Best Sellers

--- projectseven/settings.py
+++ projectseven/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^$mg=i)4$*^^s=c77*=k+(mmnf+=*ld)im)abrttcqmuugd($h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
